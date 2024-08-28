c7f2603e5bfdea3a1dd095c6ce5189cd023ad406
Olivier PEREZ <olivier@olivierperez.fr>
2024-08-28 21:31:25 +0200

    feat(level): set the right order for levels then infinite loading

--- scenes/menus/MainMenu.gd
+++ scenes/menus/MainMenu.gd
@@ -5,20 +5,18 @@ extends Control
 @onready var background: ColorRect = %Background
 @onready var seed_input: SeedInput = %SeedInput
 
-var _rng = RandomNumberGenerator.new()
-
 
 func _ready():
 	start_button.grab_focus()
 	var noise = background.material.get_shader_parameter("Noise").noise
 	var over_noise = background.material.get_shader_parameter("OverNoise").noise
-	noise.seed = _rng.randi()
-	over_noise.seed = _rng.randi()
-	seed_input.seed = _rng.randi()
+	noise.seed = randi()
+	over_noise.seed = randi()
+	seed_input.seed = randi()
 
 
 func _on_start_button_pressed():
-	Rng.set_seed(seed_input.seed)
+	seed(seed_input.seed)
 	get_tree().change_scene_to_file("res://scenes/levels/Level01.tscn")
 
 
