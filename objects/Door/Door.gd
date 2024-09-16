bd899fb15bd61ba2802abb0b690b6d8d321c1de0
Olivier PEREZ <olivier@olivierperez.fr>
2024-09-17 00:05:17 +0200

    feat(door): add sound to doors

--- objects/Door/Door.gd
+++ objects/Door/Door.gd
@@ -5,13 +5,18 @@ extends StaticBody2D
 
 @onready var sprite: AnimatedSprite2D = %Sprite
 @onready var collision: CollisionShape2D = %Collision
+@onready var open_audio: AudioStreamPlayer = %OpenAudio
+@onready var close_audio: AudioStreamPlayer = %CloseAudio
+
 
 
 func open() -> void:
 	collision.disabled = true
 	sprite.play("opened")
+	open_audio.play()
 
 
 func close() -> void:
 	collision.disabled = false
 	sprite.play("closed")
+	close_audio.play()
